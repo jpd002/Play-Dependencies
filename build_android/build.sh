299c4faf3dce72c10997b8fe0df77fed53536945
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-07-13 15:06:33 +0100

    Fix Typo:MultiOS Build Support

--- build_android/build.sh
+++ build_android/build.sh
@@ -3,11 +3,11 @@ set -e
 
 case "`uname`" in
 	CYGWIN* | MINGW*)
-		$$ANDROID_SDK_ROOT/tools/android.bat update project -p .
+		$ANDROID_SDK_ROOT/tools/android.bat update project -p .
 		$ANDROID_NDK_ROOT/ndk-build
     ;;
   Darwin* | Linux*)
-		$$ANDROID_SDK_ROOT/tools/android update project -p .
+		$ANDROID_SDK_ROOT/tools/android update project -p .
 		$ANDROID_NDK_ROOT/ndk-build
     ;;
 esac
