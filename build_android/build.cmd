356f3d1d916223e4849e3ae07e6afdbccecf3ce1
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-08-09 02:55:53 -0400

    Updated appcompat's target SDK.

--- build_android/build.cmd
+++ build_android/build.cmd
@@ -1,3 +1,3 @@
 @echo off
-CALL %ANDROID_SDK_ROOT%/tools/android.bat update project -p ../appcompat
+CALL %ANDROID_SDK_ROOT%/tools/android.bat update project -p ../appcompat -t android-22
 %ANT_HOME%/bin/ant
--- build_android/build.sh
+++ build_android/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-$ANDROID_SDK_ROOT/tools/android update project -p ../appcompat
+$ANDROID_SDK_ROOT/tools/android update project -p ../appcompat -t android-22
 $ANT_HOME/bin/ant
