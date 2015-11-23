27c72b29b053184c9b95fe36cfa5825f8ea2ccbe
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-11-22 21:23:19 -0500

    Fixed build script.

--- build_android/build.cmd
+++ build_android/build.cmd
@@ -1,3 +1,3 @@
 @echo off
-CALL %ANDROID_SDK_ROOT%/tools/android.bat update project -p ../appcompat -t android-22
+CALL %ANDROID_SDK_ROOT%/tools/android.bat update project -p ../appcompat -t android-23
 %ANT_HOME%/bin/ant
