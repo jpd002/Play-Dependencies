fc20795082254ab69475bade07595eec1c71b175
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-07-17 21:57:09 -0400

    Remove Android project update for appcompat.

--- build_android/build.cmd
+++ build_android/build.cmd
@@ -1,3 +1,2 @@
 @echo off
-CALL %ANDROID_SDK_ROOT%/tools/android.bat update project -p ../appcompat -t android-23
 %ANT_HOME%/bin/ant
--- build_android/build.sh
+++ build_android/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-$ANDROID_SDK_ROOT/tools/android update project -p ../appcompat -t android-22
 $ANT_HOME/bin/ant
